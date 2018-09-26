72d1c1c54407e3304c140384a48e2fd5da1745ab
klimson <klimson@gitlab.com>
2018-09-26 18:01:02 +0200

    Refactore cloth Class to use material enum

--- src/main/java/entity/Cloth.java
+++ src/main/java/entity/Cloth.java
@@ -1,23 +1,29 @@
 package entity;
 
+import entity.enums.Color;
+import entity.enums.Material;
+
+import java.util.EnumSet;
+
 public class Cloth extends Product {
 
     public final static char PRODUCT_TYPE = 'C';
 
     private String size;
-    private String material;
+    private Material material;
 
-    public Cloth(Long id, String productName, Float price, Float weight, String color, Integer productCount, String size, String material) {
-        super(id, productName, price, weight, color, productCount);
+    public Cloth(Long id, String productName, Float price, Float weight, EnumSet<Color> colors, Integer productCount, String size, Material material) {
+        super(id, productName, price, weight, colors, productCount);
         this.size = size;
         this.material = material;
     }
+
 
     public String getSize() {
         return size;
     }
 
-    public String getMaterial() {
+    public Material getMaterial() {
         return material;
     }
 
