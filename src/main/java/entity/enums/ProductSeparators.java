74e33df646bab1488ec203af9b1d6da987f65c3a
klimson <klimson@gitlab.com>
2018-09-26 18:17:47 +0200

    Refactore ProductParser

--- src/main/java/entity/enums/ProductSeparators.java
+++ src/main/java/entity/enums/ProductSeparators.java
@@ -2,16 +2,29 @@ package entity.enums;
 
 
 public enum ProductSeparators {
-    PRODUCT_SEPARATOR('#'), PRODUCT_ID('P'), ClOTH_ID('C'), BOOTS_ID('B');
+    PRODUCT_SEPARATOR("#"), PRODUCT_ID("P"), ClOTH_ID("C"), BOOTS_ID("B");
     
-    private char word;
+    private String word;
 
-    ProductSeparators(char word) {
+    ProductSeparators(String word) {
         this.word = word;
     }
 
     @Override
     public String toString() {
-        return String.valueOf(word);
+        return word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public static ProductSeparators getIdByChar(String word) {
+        for(ProductSeparators id: ProductSeparators.values()) {
+            if (id.getWord().equals(word)) {
+                return id;
+            }
+        }
+        return null;
     }
 }
