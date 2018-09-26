83ae2178b5bd873a876f4e18b3ae9985b92b0add
klimson <klimson@gitlab.com>
2018-09-26 18:03:19 +0200

    Refactore to use Skin types by boots class

--- src/main/java/entity/Boots.java
+++ src/main/java/entity/Boots.java
@@ -1,28 +1,33 @@
 package entity;
 
+import entity.enums.Color;
+import entity.enums.SkinType;
+
+import java.util.EnumSet;
+
 public class Boots extends Product {
 
     public final static char PRODUCT_TYPE = 'B';
 
     private Integer size;
-    private boolean isNaturalSkin;
+    private SkinType skinType;
 
-    public Boots(Long id, String productName, Float price, Float weight, String color, Integer productCount, Integer size, boolean isNaturalSkin) {
-        super(id, productName, price, weight, color, productCount);
+    public Boots(Long id, String productName, Float price, Float weight, EnumSet<Color> colors, Integer productCount, Integer size, SkinType skinType) {
+        super(id, productName, price, weight, colors, productCount);
         this.size = size;
-        this.isNaturalSkin = isNaturalSkin;
+        this.skinType = skinType;
     }
 
     public Integer getSize() {
         return size;
     }
 
-    public boolean isNaturalSkin() {
-        return isNaturalSkin;
+    public SkinType getSkinType() {
+        return skinType;
     }
 
     @Override
     public String toString() {
-        return PRODUCT_TYPE + PRODUCT_SEPARATOR + getBasicProductString() + PRODUCT_SEPARATOR + size + PRODUCT_SEPARATOR + isNaturalSkin;
+        return PRODUCT_TYPE + PRODUCT_SEPARATOR + getBasicProductString() + PRODUCT_SEPARATOR + size + PRODUCT_SEPARATOR + skinType;
     }
 }
